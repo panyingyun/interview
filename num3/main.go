09035a48aea24dd425a8572c7930e01f0126effb
panyingyun <panyingyun@gmail.com>
2019-08-13 15:12:30 +0800

    add sum3

--- num3/main.go
+++ num3/main.go
@@ -1,56 +1,57 @@
-//############题目 SUM2 ##############
-//#给定N个整数的数组S,在S中找到2个整数，使之和等于一个给定的数A（A=Si+Sj)
-//#举例：
-//#S=[12,7,11,15], target = 18
-//#因为 S[1] + S[2] =7 + 11 = 18
-//#返回 [1,2]
-
 package main
 
+//############题目 SUM3 ##############
 import (
 	"fmt"
+	"sort"
 )
 
 func main() {
-	s := []int{12, 7, 11, 15}
-	target := 18
-	ret1 := find1(s, target)
-	fmt.Println("find1 ret1 = ", ret1)
-
-	ret2 := find2(s, target)
-	fmt.Println("find2 ret2 = ", ret2)
+	a := []int{-1, 0, 1, 2, -1, -4}
+	fmt.Println(threeSum(a)) //[[-1 -1 2] [-1 0 1]]
 }
 
-//暴力破解，时间复杂度O(n^2)
-func find1(s []int, target int) []int {
-	for k1, v1 := range s {
-		for k2, v2 := range s {
-			if v1+v2 == target && k1 != k2 {
-				return []int{k1, k2}
+func threeSum(nums []int) [][]int {
+	sort.Ints(nums)
+	res := [][]int{}
+
+	for i := range nums {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+
+		l, r := i+1, len(nums)-1
+
+		for l < r {
+			s := nums[i] + nums[l] + nums[r]
+			switch {
+			case s < 0:
+				l++
+			case s > 0:
+				r--
+			default:
+				res = append(res, []int{nums[i], nums[l], nums[r]})
+				l, r = next(nums, l, r)
 			}
 		}
 	}
-	return nil
+
+	return res
 }
 
-//使用HashMap建立映射，然后使用r = target-s1, 然后查询r是否在HashMap中存在
-//HashMap Key为s1 Value为 1
-//(Key, Value) 即为 (12,0) (7,1)  (11, 2) (15,3)
-//时间复杂度 O(n)
-func find2(s []int, target int) []int {
-	//建立HashMap
-	m := make(map[int]int)
-	for i, v := range s {
-		m[v] = i
-	}
-	fmt.Println(m)
-	//遍历和查询
-	for i, v := range s {
-		r := target - v
-		k, ok := m[r]
-		if ok {
-			return []int{i, k}
+func next(nums []int, l, r int) (int, int) {
+	for l < r {
+		switch {
+		case nums[l] == nums[l+1]:
+			l++
+		case nums[r] == nums[r-1]:
+			r--
+		default:
+			l++
+			r--
+			return l, r
 		}
 	}
-	return nil
+
+	return l, r
 }
